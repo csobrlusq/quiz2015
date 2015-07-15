82455a23ca4ade759d1f3812c1ed5da1b6950610
Carlos Alberto Sobrino <casoblus.miriadax@gmail.com>
2015-07-15 15:50:14 +0200

    Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,18 +1,34 @@
 var models = require( '../models/models.js' );
-
-// GET QUESTION
-exports.question = function( req, res ) {
-	models.Quiz.findAll().then(function(quiz) {
-		res.render( '../views/quizes/question', { pregunta: quiz[0].pregunta } );
+// GET /quizes
+exports.index = function(req, res) {
+	models.Quiz.findAll().then( function( quizes ) {
+		res.render( 'quizes/index', { quizes: quizes });
 	})
 };
+
+
+
+
+// GET /quizes/:id
+exports.show = function( req, res ) {
+	models.Quiz.findById( req.params.quizId ).then( function( quiz ) {
+		res.render( 'quizes/show', { quiz: quiz } );
+	})
+};
+
+
+
+
+
 // GET ANSWER
 exports.answer = function ( req, res ) {
-	models.Quiz.findAll().then(function(quiz) {
-		if (req.query.respuesta == quiz[0].respuesta ) {
-			res.render( '../views/quizes/answer', { respuesta: 'correcta' });
+	models.Quiz.findById( req.params.quizId ).then( function( quiz ) {
+		if ( req.query.respuesta === quiz.respuesta ) {
+			res.render( 'quizes/answer', 
+				{ quiz: quiz, respuesta: 'correcta' });
 		} else {
-			res.render( '../views/quizes/answer', { respuesta: 'incorrecta' } );
+			res.render( 'quizes/answer', 
+				{ quiz: quiz, respuesta: 'incorrecta' } );
 		}
 	})
-}
+};
