fdd878de817cbd8e6f131150b730e4b222760c3a
Carlos Alberto Sobrino <casoblus.miriadax@gmail.com>
2015-07-17 18:44:01 +0200

    Formulario de búsquedas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,19 +1,38 @@
 var models = require( '../models/models.js' );
+
+// Autoload factoriza el código si ruta incluye :quizId
+exports.load = function (req, res, next, quizId) {
+	models.Quiz.findById(quizId).then(
+		function(quiz) {
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			} else {
+				next( new Error('No existe quizId = ' + quizId));
+			}
+		}
+	).catch( function( error ) { next( error ); });
+}
+
 // GET /quizes
 exports.index = function(req, res) {
-	models.Quiz.findAll().then( function( quizes ) {
-		res.render( 'quizes/index', { quizes: quizes });
-	})
+	if (req.query.search) { // Si hay un parametro search
+		// Remplaza el principio del string, los espacios, y el fin del string con % y lo guarda en la variable search
+		var search = req.query.search.replace(/^\b|\s|\b$/g, '%');
+		models.Quiz.findAll({ where: [ "pregunta like ?", search ]}).then( function( quizes ) {
+			res.render( 'quizes/index', { quizes: quizes });}
+		).catch(function(error) { next(error); });
+	} else {
+		models.Quiz.findAll().then( function( quizes ) {
+			res.render( 'quizes/index', { quizes: quizes });}
+		).catch(function(error) { next(error); });
+	}
 };
-
-
 
 
 // GET /quizes/:id
 exports.show = function( req, res ) {
-	models.Quiz.findById( req.params.quizId ).then( function( quiz ) {
-		res.render( 'quizes/show', { quiz: quiz } );
-	})
+	res.render( 'quizes/show', { quiz: req.quiz } );
 };
 
 
@@ -22,13 +41,10 @@ exports.show = function( req, res ) {
 
 // GET ANSWER
 exports.answer = function ( req, res ) {
-	models.Quiz.findById( req.params.quizId ).then( function( quiz ) {
-		if ( req.query.respuesta === quiz.respuesta ) {
-			res.render( 'quizes/answer', 
-				{ quiz: quiz, respuesta: 'correcta' });
-		} else {
-			res.render( 'quizes/answer', 
-				{ quiz: quiz, respuesta: 'incorrecta' } );
-		}
-	})
+	var resultado = 'incorrecta';
+	if ( req.query.respuesta === req.quiz.respuesta ) {
+		respuesta = 'correcta';
+	}
+	res.render( 'quizes/answer', 
+		{ quiz: req.quiz, respuesta: resultado } );
 };
